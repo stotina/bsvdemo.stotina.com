b93e5de5dc62ff02a8bf07a5cc1ee6429464d827
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2022-01-03 21:38:30 +0200

    setting up structure

---
+++ index.js
@@ -0,0 +1,13 @@
+import bsvjs from "./lib/bsv/bsv.js";
+console.log("// Using bsv.js version #" + bsvjs.version);
+
+window.help = function help() {
+  // TODO: implement
+};
+
+window.makeDataTx = function makeDataTx(
+  stringToWriteToBlockchain,
+  ...extraStringsToWrite
+) {
+  // TODO: implement
+};
